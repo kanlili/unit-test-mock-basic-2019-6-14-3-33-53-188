ce3c0e58078e5eb89fdc84c8c5a80c14f8c8290c
LILY KAN (ITA-ISDC-ISD-OOCLL/ZHA) <KANLI2@OOCL.COM>
2019-07-15 22:59:06 +0800

    Test:should_print_the_stub_purchase_when_call_process

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -11,15 +11,30 @@ public class CashRegisterTest {
     @Test
     public void should_print_the_real_purchase_when_call_process() {
         //given
+            Printer printer = mock(Printer.class);
+            CashRegister cashRegister = new CashRegister(printer);
+            Purchase purchase = new Purchase(new Item[]{});
         //when
+            cashRegister.process(purchase);
         //then
+            verify(printer, times(1)).print("");
+            verify(printer, never()).print("Not Implemented");
+
+
     }
 
     @Test
     public void should_print_the_stub_purchase_when_call_process() {
-        //given
+          //given
+          Printer printer=mock(Printer.class);
+          CashRegister cashRegister=new CashRegister(printer);
+          Purchase purchase=mock(Purchase.class);
         //when
-        //then
+          when(purchase.asString()).thenReturn("hello");
+          //then
+          cashRegister.process(purchase);
+          verify(printer,times(1)).print("hello");
+
     }
 
     @Test
